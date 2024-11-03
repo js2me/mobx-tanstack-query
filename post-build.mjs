d1a98d3c380729030efc6580c4d77b4b2df6cfc4
Sergey Volkov <js2me@outlook.com>
2024-11-03 12:30:18 +0300

    bump: update to version 2.0.10 from 2.0.9

--- post-build.mjs
+++ post-build.mjs
@@ -5,4 +5,15 @@ postBuildScript({
   rootDir: '.',
   srcDirName: 'src',
   filesToCopy: ['LICENSE', 'README.md'],
+  onPackageVersionChanged: (next, prev, { $ }) => {
+    if (process.env.PUBLISH) {
+      $('git add .');
+      $(`git commit -m "bump: update to version ${next} from ${prev}"`);
+      $('cd dist && pnpm publish && cd ..');
+      $('git push');
+      $(`git tag -a v${next} -m v${prev}`);
+      $(`git push origin v${next}`);
+      $('npm run clean');
+    }
+  }
 });
