e4d7f225ba27e3e2a247f95d59da816c97579887
Sergey Volkov <js2me@outlook.com>
2025-10-10 00:55:28 +0300

    ci: fix tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,18 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 
+import tsconfig from "./tsconfig.json";
+import { resolve } from "path";
+
+const entries = Object.entries(tsconfig.compilerOptions.paths).map(([libName, paths]) => {
+  const entryName = paths[0].replace('/index.ts', '').replace('.ts', '').replace('./src/', '');
+
+  return {
+    libName,
+    entryName: entryName === './src' ? 'index' : entryName,
+    entryPath: resolve(__dirname, paths[0]),
+  }
+})
 
 export default defineConfig({
   // @ts-ignore
@@ -19,5 +31,12 @@ export default defineConfig({
       ],
       reportsDirectory: './coverage'
     },
+  },
+  resolve: {
+    alias: Object.assign({}, ...entries.map(entry => {
+      return {
+        [entry.libName]: entry.entryPath,
+      }
+    })),
   },
 });
